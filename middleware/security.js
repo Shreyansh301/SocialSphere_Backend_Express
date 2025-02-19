0289e5fa05046f0065ab2fe6228e820b5adc1307
Shreyansh301 <shreyanshsriv5@gmail.com>
2025-02-19 12:10:49 +0530

    added middleware/ updated server.js

---
+++ middleware/security.js
@@ -0,0 +1,15 @@
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const securityMiddleware = (app) => {
+  app.use(cors({
+    origin: "https://social-sphere-vert.vercel.app",
+    credentials: true,
+  }));
+  app.use(helmet());
+  app.use(morgan('combined'));
+  app.use(express.json());
+};
+
+module.exports = securityMiddleware; 
