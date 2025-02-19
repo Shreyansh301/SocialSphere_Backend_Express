d59639d36180e0b9f6d38da45b05ff6629874b72
Shreyansh301 <shreyanshsriv5@gmail.com>
2025-02-19 12:10:05 +0530

    Added middleware and updated server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const express = require("express");
 const app = express ()
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const connectDB = require('./config/db');
+const limiter = require('./middleware/rateLimiter');
+const securityMiddleware = require('./middleware/security');
 
 
 // Connect to MongoDB
 connectDB();
+
+// Middleware
+securityMiddleware(app);
+app.use(limiter);
 
 //Routes 
 app.get("/", (req, res) => {
