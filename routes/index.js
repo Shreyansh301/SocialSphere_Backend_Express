e4c9fdb5b47bab4b3f457a3f6193440dd465126d
Shreyansh301 <shreyanshsriv5@gmail.com>
2025-02-19 12:54:20 +0530

    initialized /postRoutes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/auth', require('./authRoutes'));
 router.use('/users', require('./userRoutes'));
 router.use('/', require('./friendRoutes'));
+router.use('/', require('./postRoutes'));
 
 module.exports = router;
