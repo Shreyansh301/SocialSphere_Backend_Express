5ea3d01478bae2d5ad68a2301435d6bd35ae09c2
Shreyansh301 <shreyanshsriv5@gmail.com>
2025-02-19 12:48:15 +0530

    initialized /friendRoutes

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/auth', require('./authRoutes'));
 router.use('/users', require('./userRoutes'));
+router.use('/', require('./friendRoutes'));
 
 module.exports = router;
